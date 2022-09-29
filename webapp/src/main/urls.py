b27f7cd16d5a3e2ea2b91f2323fbb5d80f2dc3bf
Golikov Ivan <golikov244@gmail.com>
2022-09-29 19:59:32 +0200

    create base.html

--- webapp/src/main/urls.py
+++ webapp/src/main/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('news', views.news),
+    path('tournaments', views.tournaments),
+    path('freepage', views.freepage),
+    path('login', views.login),
+    path('notification', views.notification)
 ]
