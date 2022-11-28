876140397fff054a556129c2a636430391fd45c3
Ivan Golikov <golikov244@gmail.com>
2022-11-28 20:17:36 +0100

    add documentation

--- webapp/src/main/urls.py
+++ webapp/src/main/urls.py
@@ -6,11 +6,9 @@ app_name = 'main'
 urlpatterns = [
     path('', views.index, name='home'),
     path('login/', views.login, name='login'),
-    path('notification/', views.notification, name='notification'),
+    # path('notification/', views.notification, name='notification'),
     path('profile/', views.profile, name='profile'),
     path("registration/", views.registration_request, name="registration"),
     path("update_profile/", views.update_profile, name="update_profile"),
     path('user_detail/<int:user_id>', views.user_detail, name='user_detail'), 
-    
-    
 ]
